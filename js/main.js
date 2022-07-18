845a2880a0c47bb162578c6c9f2cae2aa34e0a4d
baly <kevin@baldev.it>
2022-07-18 14:10:13 +0200

    Milestone 01 Done

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// creo un array team
+// creo oggetto teamMember [name, role, photo]
+const team = [
+
+        {
+            name: 'Wayne Barnett',
+            role: 'Founder & CEO',
+            photo:'wayne-barnett-founder-ceo.jpg'
+        },
+
+        {
+            name: 'Angela Caroll',
+            role: 'Chief Editor',
+            photo:'angela-caroll-chief-editor.jpg'
+        },
+
+        {
+            name: 'Wayne Barnett',
+            role: 'Founder & CEO',
+            photo:'wayne-barnett-founder-ceo.jpg'
+        },
+
+]
+
+// milestone 01
+for (let i = 0; i < team.length; i++){
+    console.log(`Member Name: ${team[i].name} | Member role: ${team[i].role} | Member photo: ${team[i].photo}`)
+}
+
+// milestone 02
+
